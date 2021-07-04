9d7ae43e18a9eb7fc9a04dc5759a23a4e9247fa2
zhangxuet <1767002449@qq.com>
2021-07-05 03:33:02 +0800

    gridesome

---
+++ src/templates/Tag.vue
@@ -0,0 +1,61 @@
+<template>
+    <Layout>
+    
+    <!-- Page Header-->
+    <header class="masthead" style="background-image: url('/img/home-bg.jpg')">
+        <div class="container position-relative px-4 px-lg-5">
+            <div class="row gx-4 gx-lg-5 justify-content-center">
+                <div class="col-md-10 col-lg-8 col-xl-7">
+                    <div class="site-heading">
+                        <h1>{{ $page.tag.title }}</h1>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
+    <!-- Main Content-->
+    <div class="container px-4 px-lg-5">
+        <div class="row gx-4 gx-lg-5 justify-content-center">
+            <div class="col-md-10 col-lg-8 col-xl-7">
+                <!-- Post preview-->
+                <div class="post-preview" v-for="post in $page.tag.posts" :key="post.id">
+                    <g-link :to="'/post/'+post.id">
+                        <h2 class="post-title">{{post.title}}</h2>
+                        <!-- <h3 class="post-subtitle">Problems look mighty small from 150 miles up</h3> -->
+                    </g-link>
+                    <p class="post-meta">
+                        Posted by
+                        <g-link>Start Bootstrap</g-link>
+                        on{{post.created_at}}
+                    </p>
+                    <hr class="my-4" />
+                </div>
+                <!-- Pager-->
+                <!-- <div class="d-flex justify-content-end mb-4"><a class="btn btn-primary text-uppercase" href="#!">Older Posts →</a></div> -->
+                <!-- <Pager :info="$page.posts.pageInfo"/> -->
+            </div>
+        </div>
+    </div>
+    
+  </Layout>
+</template>
+
+<page-query>
+    query($id: ID!) {
+        tag: strapiTag (id: $id) {
+            id
+            title
+            posts {
+                id
+                title
+                created_at
+            }
+        }
+    }
+</page-query>
+
+<script>
+export default {
+    name: 'TagPage'
+}
+</script>
